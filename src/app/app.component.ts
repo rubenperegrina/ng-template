aa42cbab6a7151dca8cde665b899ba228be542eb
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-07-04 12:31:52 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-template';
+  cities: string[] = ['Barcelona', 'Bangkok', 'Madrid', 'Tokyo']
 }
